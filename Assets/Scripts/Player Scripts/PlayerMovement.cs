b4296791bf4acaa572915f3300bbb1f0ee5e449f
AngelikaJustine <fay.ariana@gmail.com>
2020-05-25 20:53:21 +0700

    Update Input for Movement

--- Assets/Scripts/Player Scripts/PlayerMovement.cs
+++ Assets/Scripts/Player Scripts/PlayerMovement.cs
@@ -2,32 +2,51 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerMovement : MonoBehaviour {
+public class PlayerMovement : MonoBehaviour
+{
 
     private Rigidbody2D myBody;
 
     public float moveSpeed = 2f;
 
-    void Awake() {
+    void Awake()
+    {
         myBody = GetComponent<Rigidbody2D>();
     }
 
-    void FixedUpdate() {
+    void FixedUpdate()
+    {
         Move();
     }
 
-    void Move() {
-     
-        if(Input.GetAxisRaw("Horizontal") > 0f) {
-            myBody.velocity = new Vector2(moveSpeed, myBody.velocity.y);
-        }
+    void Move()
+    {
 
-        if (Input.GetAxisRaw("Horizontal") < 0f) {
-            myBody.velocity = new Vector2(-moveSpeed, myBody.velocity.y);
+        // if(Input.GetAxisRaw("Horizontal") > 0f) {
+        //     myBody.velocity = new Vector2(moveSpeed, myBody.velocity.y);
+        // }
+
+        // if (Input.GetAxisRaw("Horizontal") < 0f) {
+        //     myBody.velocity = new Vector2(-moveSpeed, myBody.velocity.y);
+        // }
+
+        if (Input.touchCount > 0)
+        {
+            Touch touch = Input.GetTouch(0);
+
+            if (touch.position.x < Screen.width / 2)
+            {
+                myBody.velocity = new Vector2(-moveSpeed, myBody.velocity.y);
+            }
+            if (touch.position.x > Screen.width / 2)
+            {
+                myBody.velocity = new Vector2(moveSpeed, myBody.velocity.y);
+            }
         }
     } // move
 
-    public void PlatformMove(float x) {
+    public void PlatformMove(float x)
+    {
         myBody.velocity = new Vector2(x, myBody.velocity.y);
     }
 
