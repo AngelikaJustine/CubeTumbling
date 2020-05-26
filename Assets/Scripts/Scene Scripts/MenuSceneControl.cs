0724cc6a9286600ae450e0073540abe8313ad790
feliciafunay <felicia.funay@gmail.com>
2020-05-26 16:37:56 +0700

    perbaiki UI scene gameplay dan gameover

--- Assets/Scripts/Scene Scripts/MenuSceneControl.cs
+++ Assets/Scripts/Scene Scripts/MenuSceneControl.cs
@@ -1,29 +1,15 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.SceneManagement;
-
 
 public class MenuSceneControl : MonoBehaviour
 {
-    // int sceneIndex;
-
-    // Start is called before the first frame update
-    // void Start()
-    // {
-    //     sceneIndex = SceneManager.GetActiveScene().buildIndex;    
-    // }
-
     // Update is called once per frame
     void Update()
     {
         if (Input.GetKeyDown(KeyCode.Escape))
             Application.Quit();
     }
-
-    // public void LoadNextLevel(){
-    //     SceneManager.LoadScene(sceneIndex+1);
-    // }
 
     public void changeMenuScene(string sceneName)
     {
